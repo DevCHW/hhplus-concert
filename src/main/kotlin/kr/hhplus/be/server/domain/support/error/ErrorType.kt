4194d086596099405127a5c83f980fb42f48f8db
DevCHW <ggoma003@naver.com>
2025-01-17 14:56:49 +0900

    feat: Token 에러타입 추가

--- src/main/kotlin/kr/hhplus/be/server/domain/support/error/ErrorType.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/support/error/ErrorType.kt
@@ -9,6 +9,10 @@ enum class ErrorType(
     val message: String,
     val logLevel: LogLevel,
 ) {
+    TOKEN_EMPTY(HttpStatus.BAD_REQUEST, "토큰 값이 존재하지 않습니다.", LogLevel.WARN),
+    TOKEN_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 발급받은 토큰이 존재합니다.", LogLevel.WARN),
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰입니다.", LogLevel.WARN),
+
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다.", LogLevel.ERROR),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.", LogLevel.WARN),
 }
