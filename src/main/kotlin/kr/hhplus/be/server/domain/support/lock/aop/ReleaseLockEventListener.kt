19ced3c5294429a2d413a53e3e69587b287860b5
DevCHW <ggoma003@naver.com>
2025-01-24 16:15:18 +0900

    fix: 락 해제 이벤트 리스너의 phase 속성을 AFTER_COMMIT -> AFTER_COMPLETION으로 수정

--- src/main/kotlin/kr/hhplus/be/server/domain/support/lock/aop/ReleaseLockEventListener.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/support/lock/aop/ReleaseLockEventListener.kt
@@ -3,7 +3,6 @@ package kr.hhplus.be.server.domain.support.lock.aop
 import kr.hhplus.be.server.domain.support.lock.LockResourceManager
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
@@ -13,8 +12,7 @@ class ReleaseLockEventListener {
 
     private val log: Logger = LoggerFactory.getLogger(javaClass)
 
-    @EventListener
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
     fun releaseLock(lockResourceManager: LockResourceManager) {
         try {
             lockResourceManager.unlock()
